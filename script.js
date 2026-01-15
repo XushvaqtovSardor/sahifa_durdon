a9ba0447cd3af9cdee2c05378d9a21732bc01bcb
fn25 <xushvaqtovsardor032@gmail.com>
2026-01-15 17:37:04 +0500

    fix

---
+++ script.js
@@ -0,0 +1,7 @@
+function buy() {
+    alert("🌸 Buyurtmangiz muvaffaqiyatli qabul qilindi!");
+}
+function sendMessage(event) {
+    event.preventDefault();
+    alert("✅ Xabaringiz yuborildi! Tez orada bog‘lanamiz 🌸");
+}
